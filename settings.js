206447223ff31491353ed13751d532951b77f35c
𝐄𝐦𝐩𝐢𝐫𝐞 𝐓𝐞𝐜𝐡 [ 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 ] <151479661+efeurhobobullish@users.noreply.github.com>
2025-02-08 23:33:09 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'mpire Tech'// yourname
+global.MONGO_DB_URI = "mongodb+srv://empiretech:kkVDB0ooTlgIsH11@cluster0.js7ra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "empiretechapi@gmail.com" //email
+global.email_password = "fxlq bhap skup nssi" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
